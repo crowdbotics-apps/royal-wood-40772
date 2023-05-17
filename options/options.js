c503a292c4d417094ab4aa3ed39220aac0714c60
Crowdbotics <team@crowdbotics.com>
2023-05-17 15:37:48 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "royal-wood-40772",
+  url: "https://royal-wood-40772.botics.co",
+  api: "https://royal-wood-40772.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
